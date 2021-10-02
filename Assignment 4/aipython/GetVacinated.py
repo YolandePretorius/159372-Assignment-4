3cfd915de04569dc5034049141ef38f701e2f8f3
Yolande <yolande.y.kok@gmail.com>
2021-10-02 19:54:27 +1300

    First commit

---
+++ Assignment 4/aipython/GetVacinated.py
@@ -0,0 +1,17 @@
+'''
+Created on 1/10/2021
+
+@author: yolan
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
